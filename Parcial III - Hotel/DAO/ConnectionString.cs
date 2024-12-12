89808c5dd82062acda6e8889897da9a4a6ff2a98
Juan Alatorre <reaperplay729@gmail.com>
2024-12-12 11:24:44 -0700

    crear reservacion y consultar habitaciones disponibles alv

---
+++ Parcial III - Hotel/DAO/ConnectionString.cs
@@ -0,0 +1,7 @@
+﻿namespace Parcial_III___Hotel.DAO
+{
+    public class ConnectionString
+    {
+        protected static readonly string connectionString = "Server=localhost;Database=SuiteSavage;Uid=root;Pwd=0000";
+    }
+}
