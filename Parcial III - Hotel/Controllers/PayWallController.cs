729df65c23413cc1905bd65c9b335b107991ec7f
Laptop Santiago <castanossantiago102@gmail.com>
2024-12-16 17:50:06 -0700

    Implementaciones para la PayWall.

--- Parcial III - Hotel/Controllers/PayWallController.cs
+++ Parcial III - Hotel/Controllers/PayWallController.cs
@@ -8,17 +8,20 @@ namespace Parcial_III___Hotel.Controllers
     public class PayWallController
     {
         private FrmPayWall _frmPayWall;
+        private decimal aPagar;
+
         public PayWallController(FrmPayWall frmPayWall)
         {
             _frmPayWall = frmPayWall;
             _frmPayWall.btnPayWall_Pagar.Click += BtnPayWall_Pagar_Click;
             _frmPayWall.cmboxPayWall_TipoPago.SelectedIndexChanged += cmboxPayWall_TipoPago_SelectedIndexChanged;
+            _frmPayWall.msktxtboxPayWall_Pago.KeyDown += msktxtboxPayWall_Pago_KeyDown;
         }
 
         public void MostrarPago(decimal idCheck)
         {
-            decimal cantidadPagar = PayWallDAO.CantidadPagar(idCheck);
-            _frmPayWall.lblPayWall_CantidadPagar.Text = $"${cantidadPagar}";
+            aPagar = PayWallDAO.CantidadPagar(idCheck);
+            _frmPayWall.lblPayWall_CantidadPagar.Text = $"${aPagar}";
             _frmPayWall.ShowDialog();
         }
 
@@ -28,25 +31,80 @@ namespace Parcial_III___Hotel.Controllers
             {
                 _frmPayWall.lblPayWall_InfoTarjeta.Visible = true;
                 _frmPayWall.lblPayWall_NumT.Visible = true;
-                _frmPayWall.lblPayWall_FechaVCVV.Visible = true;
+                _frmPayWall.lblPayWall_FechaV.Visible = true;
+                _frmPayWall.lblPayWall_CVV.Visible = true;
                 _frmPayWall.msktxtboxPayWall_NumTarjeta.Visible = true;
                 _frmPayWall.msktxtboxPayWall_FechaV.Visible = true;
                 _frmPayWall.msktxtboxPayWall_CVV.Visible = true;
+
+                _frmPayWall.lblPayWall_Pago.Visible = false;
+                _frmPayWall.lblPayWall_Cambio.Visible = false;
+                _frmPayWall.msktxtboxPayWall_Pago.Visible = false;
+                _frmPayWall.lblPayWall_Vuelto.Visible = false;
             }
-            else
+            else if (_frmPayWall.cmboxPayWall_TipoPago.Text == "Efectivo")
             {
+                _frmPayWall.lblPayWall_Pago.Visible = true;
+                _frmPayWall.lblPayWall_Cambio.Visible = true;
+                _frmPayWall.msktxtboxPayWall_Pago.Visible = true;
+                _frmPayWall.lblPayWall_Vuelto.Visible = true;
+
                 _frmPayWall.lblPayWall_InfoTarjeta.Visible = false;
                 _frmPayWall.lblPayWall_NumT.Visible = false;
-                _frmPayWall.lblPayWall_FechaVCVV.Visible = false;
+                _frmPayWall.lblPayWall_FechaV.Visible = false;
+                _frmPayWall.lblPayWall_CVV.Visible = false;
                 _frmPayWall.msktxtboxPayWall_NumTarjeta.Visible = false;
                 _frmPayWall.msktxtboxPayWall_FechaV.Visible = false;
                 _frmPayWall.msktxtboxPayWall_CVV.Visible = false;
+            }
+
+        }
+
+        private void msktxtboxPayWall_Pago_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Verificar si la tecla presionada es "Enter"
+            if (e.KeyCode == Keys.Enter)
+            {
+                _frmPayWall.lblPayWall_Vuelto.Text = "";  // Limpiar el mensaje de vuelto
+
+                // Llamar a la función CalcularCambio pasando el valor de aPagar
+                CalcularCambio(aPagar);
+
+                // Marcar el evento como manejado para evitar que cause otras acciones
+                e.Handled = true;
+            }
+        }
+
+        private void CalcularCambio(decimal aPagar) 
+        {
+            if (decimal.TryParse(_frmPayWall.msktxtboxPayWall_Pago.Text, out decimal montoPagado))
+            {
+                if (montoPagado < aPagar)
+                {
+                    MessageBox.Show("La cantidad pagada es menor que el monto total a pagar.");
+                    _frmPayWall.btnPayWall_Pagar.Enabled = false;
+                }
+                else if (montoPagado == aPagar)
+                {
+                    _frmPayWall.lblPayWall_Vuelto.Text = "Pago Exacto.";
+                    _frmPayWall.btnPayWall_Pagar.Enabled = true;
+                }
+                else
+                {
+                    decimal cambio = montoPagado - aPagar;
+                    _frmPayWall.lblPayWall_Vuelto.Text = "El cambio a devolver es: " + cambio.ToString("C");
+                    _frmPayWall.btnPayWall_Pagar.Enabled = true;
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingresa un monto válido.");
             }
         }
 
         private void BtnPayWall_Pagar_Click(object? sender, EventArgs e)
         {
-            MessageBox.Show(_frmPayWall.msktxtboxPayWall_NumTarjeta.Text);
             _frmPayWall.Close();
         }
     }
--- Parcial III - Hotel/Views/FrmPayWall.Designer.cs
+++ Parcial III - Hotel/Views/FrmPayWall.Designer.cs
@@ -37,14 +37,19 @@
             msktxtboxPayWall_NumTarjeta = new MaskedTextBox();
             msktxtboxPayWall_FechaV = new MaskedTextBox();
             lblPayWall_NumT = new Label();
-            lblPayWall_FechaVCVV = new Label();
+            lblPayWall_FechaV = new Label();
             msktxtboxPayWall_CVV = new MaskedTextBox();
+            lblPayWall_CVV = new Label();
+            msktxtboxPayWall_Pago = new MaskedTextBox();
+            lblPayWall_Pago = new Label();
+            lblPayWall_Cambio = new Label();
+            lblPayWall_Vuelto = new Label();
             SuspendLayout();
             // 
             // btnPayWall_Pagar
             // 
             btnPayWall_Pagar.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnPayWall_Pagar.Location = new Point(237, 365);
+            btnPayWall_Pagar.Location = new Point(285, 373);
             btnPayWall_Pagar.Name = "btnPayWall_Pagar";
             btnPayWall_Pagar.Size = new Size(211, 38);
             btnPayWall_Pagar.TabIndex = 0;
@@ -55,7 +60,7 @@
             // 
             lblPayWall_InfoPago.AutoSize = true;
             lblPayWall_InfoPago.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblPayWall_InfoPago.Location = new Point(24, 23);
+            lblPayWall_InfoPago.Location = new Point(30, 123);
             lblPayWall_InfoPago.Name = "lblPayWall_InfoPago";
             lblPayWall_InfoPago.Size = new Size(176, 30);
             lblPayWall_InfoPago.TabIndex = 1;
@@ -65,7 +70,7 @@
             // 
             lblPayWall_CantidadPagar.AutoSize = true;
             lblPayWall_CantidadPagar.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblPayWall_CantidadPagar.Location = new Point(223, 23);
+            lblPayWall_CantidadPagar.Location = new Point(30, 153);
             lblPayWall_CantidadPagar.Name = "lblPayWall_CantidadPagar";
             lblPayWall_CantidadPagar.Size = new Size(0, 30);
             lblPayWall_CantidadPagar.TabIndex = 2;
@@ -73,20 +78,21 @@
             // cmboxPayWall_TipoPago
             // 
             cmboxPayWall_TipoPago.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmboxPayWall_TipoPago.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             cmboxPayWall_TipoPago.FormattingEnabled = true;
             cmboxPayWall_TipoPago.Items.AddRange(new object[] { "Efectivo", "Tarjeta" });
-            cmboxPayWall_TipoPago.Location = new Point(24, 107);
+            cmboxPayWall_TipoPago.Location = new Point(30, 65);
             cmboxPayWall_TipoPago.Name = "cmboxPayWall_TipoPago";
-            cmboxPayWall_TipoPago.Size = new Size(123, 23);
+            cmboxPayWall_TipoPago.Size = new Size(123, 29);
             cmboxPayWall_TipoPago.TabIndex = 3;
             // 
             // lblPayWall_InfoTarjeta
             // 
             lblPayWall_InfoTarjeta.AutoSize = true;
-            lblPayWall_InfoTarjeta.Font = new Font("Corbel", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblPayWall_InfoTarjeta.Location = new Point(24, 158);
+            lblPayWall_InfoTarjeta.Font = new Font("Corbel", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPayWall_InfoTarjeta.Location = new Point(382, 32);
             lblPayWall_InfoTarjeta.Name = "lblPayWall_InfoTarjeta";
-            lblPayWall_InfoTarjeta.Size = new Size(222, 26);
+            lblPayWall_InfoTarjeta.Size = new Size(309, 36);
             lblPayWall_InfoTarjeta.TabIndex = 4;
             lblPayWall_InfoTarjeta.Text = "Información de la Tarjeta";
             lblPayWall_InfoTarjeta.Visible = false;
@@ -95,7 +101,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(24, 74);
+            label1.Location = new Point(30, 18);
             label1.Name = "label1";
             label1.Size = new Size(167, 30);
             label1.TabIndex = 5;
@@ -104,7 +110,7 @@
             // msktxtboxPayWall_NumTarjeta
             // 
             msktxtboxPayWall_NumTarjeta.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            msktxtboxPayWall_NumTarjeta.Location = new Point(24, 231);
+            msktxtboxPayWall_NumTarjeta.Location = new Point(432, 133);
             msktxtboxPayWall_NumTarjeta.Mask = "0000-0000-0000-0000";
             msktxtboxPayWall_NumTarjeta.Name = "msktxtboxPayWall_NumTarjeta";
             msktxtboxPayWall_NumTarjeta.PromptChar = 'X';
@@ -116,7 +122,7 @@
             // msktxtboxPayWall_FechaV
             // 
             msktxtboxPayWall_FechaV.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            msktxtboxPayWall_FechaV.Location = new Point(24, 269);
+            msktxtboxPayWall_FechaV.Location = new Point(432, 179);
             msktxtboxPayWall_FechaV.Mask = "00/00";
             msktxtboxPayWall_FechaV.Name = "msktxtboxPayWall_FechaV";
             msktxtboxPayWall_FechaV.PromptChar = 'X';
@@ -128,28 +134,28 @@
             // 
             lblPayWall_NumT.AutoSize = true;
             lblPayWall_NumT.Font = new Font("Corbel", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblPayWall_NumT.Location = new Point(24, 195);
+            lblPayWall_NumT.Location = new Point(432, 97);
             lblPayWall_NumT.Name = "lblPayWall_NumT";
             lblPayWall_NumT.Size = new Size(157, 33);
             lblPayWall_NumT.TabIndex = 9;
             lblPayWall_NumT.Text = "Num. Tarjeta";
             lblPayWall_NumT.Visible = false;
             // 
-            // lblPayWall_FechaVCVV
+            // lblPayWall_FechaV
             // 
-            lblPayWall_FechaVCVV.AutoSize = true;
-            lblPayWall_FechaVCVV.Font = new Font("Corbel", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblPayWall_FechaVCVV.Location = new Point(12, 311);
-            lblPayWall_FechaVCVV.Name = "lblPayWall_FechaVCVV";
-            lblPayWall_FechaVCVV.Size = new Size(225, 26);
-            lblPayWall_FechaVCVV.TabIndex = 10;
-            lblPayWall_FechaVCVV.Text = "Fecha Vencimiento / CVV";
-            lblPayWall_FechaVCVV.Visible = false;
+            lblPayWall_FechaV.AutoSize = true;
+            lblPayWall_FechaV.Font = new Font("Corbel", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPayWall_FechaV.Location = new Point(422, 253);
+            lblPayWall_FechaV.Name = "lblPayWall_FechaV";
+            lblPayWall_FechaV.Size = new Size(248, 26);
+            lblPayWall_FechaV.TabIndex = 10;
+            lblPayWall_FechaV.Text = "Fecha Vencimiento  MM/AA";
+            lblPayWall_FechaV.Visible = false;
             // 
             // msktxtboxPayWall_CVV
             // 
             msktxtboxPayWall_CVV.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            msktxtboxPayWall_CVV.Location = new Point(94, 269);
+            msktxtboxPayWall_CVV.Location = new Point(500, 179);
             msktxtboxPayWall_CVV.Mask = "000/0";
             msktxtboxPayWall_CVV.Name = "msktxtboxPayWall_CVV";
             msktxtboxPayWall_CVV.PromptChar = 'X';
@@ -157,13 +163,72 @@
             msktxtboxPayWall_CVV.TabIndex = 11;
             msktxtboxPayWall_CVV.Visible = false;
             // 
+            // lblPayWall_CVV
+            // 
+            lblPayWall_CVV.AutoSize = true;
+            lblPayWall_CVV.Font = new Font("Corbel", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPayWall_CVV.Location = new Point(559, 182);
+            lblPayWall_CVV.Name = "lblPayWall_CVV";
+            lblPayWall_CVV.Size = new Size(50, 26);
+            lblPayWall_CVV.TabIndex = 12;
+            lblPayWall_CVV.Text = "CVV";
+            lblPayWall_CVV.Visible = false;
+            // 
+            // msktxtboxPayWall_Pago
+            // 
+            msktxtboxPayWall_Pago.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            msktxtboxPayWall_Pago.Location = new Point(30, 263);
+            msktxtboxPayWall_Pago.Mask = "00000";
+            msktxtboxPayWall_Pago.Name = "msktxtboxPayWall_Pago";
+            msktxtboxPayWall_Pago.Size = new Size(94, 29);
+            msktxtboxPayWall_Pago.TabIndex = 13;
+            msktxtboxPayWall_Pago.Visible = false;
+            // 
+            // lblPayWall_Pago
+            // 
+            lblPayWall_Pago.AutoSize = true;
+            lblPayWall_Pago.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPayWall_Pago.Location = new Point(30, 222);
+            lblPayWall_Pago.Name = "lblPayWall_Pago";
+            lblPayWall_Pago.Size = new Size(103, 30);
+            lblPayWall_Pago.TabIndex = 14;
+            lblPayWall_Pago.Text = "Paga con:";
+            lblPayWall_Pago.Visible = false;
+            // 
+            // lblPayWall_Cambio
+            // 
+            lblPayWall_Cambio.AutoSize = true;
+            lblPayWall_Cambio.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPayWall_Cambio.Location = new Point(30, 327);
+            lblPayWall_Cambio.Name = "lblPayWall_Cambio";
+            lblPayWall_Cambio.Size = new Size(193, 30);
+            lblPayWall_Cambio.TabIndex = 16;
+            lblPayWall_Cambio.Text = "Cambio a Devolver:";
+            lblPayWall_Cambio.Visible = false;
+            // 
+            // lblPayWall_Vuelto
+            // 
+            lblPayWall_Vuelto.AutoSize = true;
+            lblPayWall_Vuelto.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPayWall_Vuelto.Location = new Point(30, 373);
+            lblPayWall_Vuelto.Name = "lblPayWall_Vuelto";
+            lblPayWall_Vuelto.Size = new Size(0, 21);
+            lblPayWall_Vuelto.TabIndex = 17;
+            lblPayWall_Vuelto.Visible = false;
+            // 
             // FrmPayWall
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(800, 450);
+            Controls.Add(lblPayWall_Vuelto);
+            Controls.Add(lblPayWall_Cambio);
+            Controls.Add(lblPayWall_Pago);
+            Controls.Add(msktxtboxPayWall_Pago);
+            Controls.Add(lblPayWall_CVV);
             Controls.Add(msktxtboxPayWall_CVV);
-            Controls.Add(lblPayWall_FechaVCVV);
+            Controls.Add(lblPayWall_FechaV);
             Controls.Add(lblPayWall_NumT);
             Controls.Add(msktxtboxPayWall_FechaV);
             Controls.Add(msktxtboxPayWall_NumTarjeta);
@@ -174,7 +239,7 @@
             Controls.Add(lblPayWall_InfoPago);
             Controls.Add(btnPayWall_Pagar);
             Name = "FrmPayWall";
-            Text = "FrmPayWall";
+            Text = "Formulario De Pago";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -190,7 +255,12 @@
         public MaskedTextBox msktxtboxPayWall_NumTarjeta;
         public MaskedTextBox msktxtboxPayWall_FechaV;
         public Label lblPayWall_NumT;
-        public Label lblPayWall_FechaVCVV;
+        public Label lblPayWall_FechaV;
         public MaskedTextBox msktxtboxPayWall_CVV;
+        public Label lblPayWall_CVV;
+        public MaskedTextBox msktxtboxPayWall_Pago;
+        public Label lblPayWall_Pago;
+        public Label lblPayWall_Cambio;
+        public Label lblPayWall_Vuelto;
     }
 }
